018b60911d12b706db54e64e55ef50777df2d3fb
Fadli <ahmadfadlyramadhan@gmail.com>
2024-03-12 04:46:50 +0800

    using cache on dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -67,69 +67,73 @@ def linechart(df):
     st.pyplot(fig)
 
 
-df_qualityair = load_csv(
-    "https://raw.githubusercontent.com/AhmadFadliRamadhan/submission-01-bangkit/main/dashboard/df_qualityair.csv"
-)
-df_variable = load_csv(
-    "https://raw.githubusercontent.com/AhmadFadliRamadhan/submission-01-bangkit/main/dashboard/df_variable.csv"
-)
-
-# Filter data
-min_date, max_date = min_max(df_qualityair)
-
-
-with st.sidebar:
-    # Menambahkan logo perusahaan
-    st.image(
-        "https://raw.githubusercontent.com/AhmadFadliRamadhan/submission-01-bangkit/main/dashboard/logo.png",
-        width=200,
+def main():
+    df_qualityair = load_csv(
+        "https://raw.githubusercontent.com/AhmadFadliRamadhan/submission-01-bangkit/main/dashboard/df_qualityair.csv"
+    )
+    df_variable = load_csv(
+        "https://raw.githubusercontent.com/AhmadFadliRamadhan/submission-01-bangkit/main/dashboard/df_variable.csv"
     )
 
-    # Mengambil start_date & end_date dari date_input
-    start_date, end_date = st.date_input(
-        label="Rentang Waktu",
-        min_value=min_date,
-        max_value=max_date,
-        value=[min_date, max_date],
-    )
+    # Filter data
+    min_date, max_date = min_max(df_qualityair)
 
-df_a = filter(df_qualityair, start_date, end_date)
-df_b = filter(df_variable, start_date, end_date)
+    with st.sidebar:
+        # Menambahkan logo perusahaan
+        st.image(
+            "https://raw.githubusercontent.com/AhmadFadliRamadhan/submission-01-bangkit/main/dashboard/logo.png",
+            width=200,
+        )
 
-st.title("Submission 1 | Ahmad Fadli Ramadhan")
+        # Mengambil start_date & end_date dari date_input
+        start_date, end_date = st.date_input(
+            label="Rentang Waktu",
+            min_value=min_date,
+            max_value=max_date,
+            value=[min_date, max_date],
+        )
 
-st.header("Air Quality Dataset")
+    df_a = filter(df_qualityair, start_date, end_date)
+    df_b = filter(df_variable, start_date, end_date)
 
-st.write("##### Scatter Plot Matrix Parameter Kualitas Udara di Stasiun Dingling")
+    st.title("Submission 1 | Ahmad Fadli Ramadhan")
 
-scatterplot(df_qualityair)
+    st.header("Air Quality Dataset")
 
-st.write(
-    """
-         Berdasarkan visualisasi scatter matriks di atas, diketahui bahwa:
+    st.write("##### Scatter Plot Matrix Parameter Kualitas Udara di Stasiun Dingling")
 
-        - `PM2.5` dan `PM10` terlihat dengan jelas membentuk sebuah garis mengarah ke kanan atas, menunjukkan korelasi positif yang kuat.
-        - `CO` dan `NO2` membentuk persebaran mengarah ke kanan atas, persebarannya agak tersebar, menunjukkan korelasi positif yang sedang.
-        - `CO` - `PM2.5`, `NO2` - `PM2.5`, `CO` - `PM10`, dan `NO2` - `PM10` membentuk persebaran mengarah ke kanan atas walau tidak terlalu condong, memiliki persebaran yang luas, menunjukkan korelasi positif yang lemah.
-        - Sisa pasangan yang tidak disebutkan memiliki persebaran yang acak.
-         """
-)
+    scatterplot(df_qualityair)
 
-st.write(
-    "##### Grafik Rata-Rata Nilai Tiap Kolom per Hari di Stasiun Pemantauan Dingling"
-)
-
-st.write(
-    """
-        Berdasarkan visualisasi di atas, diketahui bahwa:
-
-        - `TEMP`, `PRES`, dan `DEWP` memiliki pola musiman, di mana `TEMP` dan `DEWP` memiliki pola garis yang cenderung mirip.
-        - `PRES` terhadap `TEMP` dan `DEWP` memiliki pola garis yang berlawanan.
-        - `RAIN` memiliki pola garis lurus yang cenderung konstan pada nilai nol.
+    st.write(
         """
-)
+            Berdasarkan visualisasi scatter matriks di atas, diketahui bahwa:
 
-daily_mean = groupbydate(df_variable)
-linechart(daily_mean)
+            - `PM2.5` dan `PM10` terlihat dengan jelas membentuk sebuah garis mengarah ke kanan atas, menunjukkan korelasi positif yang kuat.
+            - `CO` dan `NO2` membentuk persebaran mengarah ke kanan atas, persebarannya agak tersebar, menunjukkan korelasi positif yang sedang.
+            - `CO` - `PM2.5`, `NO2` - `PM2.5`, `CO` - `PM10`, dan `NO2` - `PM10` membentuk persebaran mengarah ke kanan atas walau tidak terlalu condong, memiliki persebaran yang luas, menunjukkan korelasi positif yang lemah.
+            - Sisa pasangan yang tidak disebutkan memiliki persebaran yang acak.
+            """
+    )
 
-st.caption("Oleh Ahmad Fadli Ramadhan")
+    st.write(
+        "##### Grafik Rata-Rata Nilai Tiap Kolom per Hari di Stasiun Pemantauan Dingling"
+    )
+
+    st.write(
+        """
+            Berdasarkan visualisasi di atas, diketahui bahwa:
+
+            - `TEMP`, `PRES`, dan `DEWP` memiliki pola musiman, di mana `TEMP` dan `DEWP` memiliki pola garis yang cenderung mirip.
+            - `PRES` terhadap `TEMP` dan `DEWP` memiliki pola garis yang berlawanan.
+            - `RAIN` memiliki pola garis lurus yang cenderung konstan pada nilai nol.
+            """
+    )
+
+    daily_mean = groupbydate(df_variable)
+    linechart(daily_mean)
+
+    st.caption("Oleh Ahmad Fadli Ramadhan")
+
+
+if __name__ == "__main__":
+    main()
