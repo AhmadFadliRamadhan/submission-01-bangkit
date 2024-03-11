6bd7e7fda2485eb164cfdd4d234e4edf316a150b
Fadli <ahmadfadlyramadhan@gmail.com>
2024-03-12 04:44:01 +0800

    using cache on dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,19 +2,81 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 
-df_qualityair = pd.read_csv(
+
+@st.cache_data
+def load_csv(url):
+    df = pd.read_csv(url)
+    df["date"] = pd.to_datetime(df["date"])
+    return df
+
+
+@st.cache_data
+def filter(df, start, end):
+    return df[(df["date"] >= str(start)) & (df["date"] <= str(end))]
+
+
+@st.cache_data
+def min_max(df):
+    return df["date"].min(), df["date"].max()
+
+
+@st.cache_data
+def groupbydate(df):
+    return df.groupby(df["date"].dt.date).mean()
+
+
+@st.cache_data
+def scatterplot(df):
+    # Membuat scatter matrix
+    scatter_data = df[["PM2.5", "PM10", "SO2", "NO2", "CO", "O3"]]
+    scatter_matrix = pd.plotting.scatter_matrix(
+        scatter_data, figsize=(12, 12), diagonal="kde"
+    )
+
+    # Memodifikasi plot
+    for i in range(len(scatter_data.columns)):
+        for j in range(len(scatter_data.columns)):
+            ax = scatter_matrix[i, j]
+            ax.xaxis.label.set_rotation(45)
+            ax.yaxis.label.set_rotation(0)
+            ax.yaxis.label.set_ha("right")
+            ax.xaxis.label.set_size(10)
+            ax.yaxis.label.set_size(10)
+
+    # Menambah judul
+    scatter_matrix[0, 0].get_figure().suptitle(
+        "Scatter Plot Matrix Parameter Kualitas Udara di Stasiun Pemantauan Dingling"
+    )
+
+    # Menampilkan plot di Streamlit
+    st.pyplot(scatter_matrix[0, 0].get_figure())
+
+
+def linechart(df):
+    fig, ax = plt.subplots(figsize=(18, 6))
+    for column in df.columns:
+        if column != "date":
+            ax.plot(df.index, df[column], label=column)
+
+    ax.set_title(
+        "Grafik Rata-Rata Nilai Tiap Kolom per Hari di Stasiun Pemantauan Dingling"
+    )
+    ax.set_xlabel("Tanggal")
+    ax.set_ylabel("Nilai Rata-Rata")
+    ax.legend()
+    st.pyplot(fig)
+
+
+df_qualityair = load_csv(
     "https://raw.githubusercontent.com/AhmadFadliRamadhan/submission-01-bangkit/main/dashboard/df_qualityair.csv"
 )
-df_qualityair["date"] = pd.to_datetime(df_qualityair["date"])
-
-df_variable = pd.read_csv(
+df_variable = load_csv(
     "https://raw.githubusercontent.com/AhmadFadliRamadhan/submission-01-bangkit/main/dashboard/df_variable.csv"
 )
-df_variable["date"] = pd.to_datetime(df_variable["date"])
 
 # Filter data
-min_date = df_qualityair["date"].min()
-max_date = df_qualityair["date"].max()
+min_date, max_date = min_max(df_qualityair)
+
 
 with st.sidebar:
     # Menambahkan logo perusahaan
@@ -31,15 +93,8 @@ with st.sidebar:
         value=[min_date, max_date],
     )
 
-df_a = df_qualityair[
-    (df_qualityair["date"] >= str(start_date))
-    & (df_qualityair["date"] <= str(end_date))
-]
-
-df_b = df_variable[
-    (df_variable["date"] >= str(start_date)) & (df_variable["date"] <= str(end_date))
-]
-
+df_a = filter(df_qualityair, start_date, end_date)
+df_b = filter(df_variable, start_date, end_date)
 
 st.title("Submission 1 | Ahmad Fadli Ramadhan")
 
@@ -47,9 +102,11 @@ st.header("Air Quality Dataset")
 
 st.write("##### Scatter Plot Matrix Parameter Kualitas Udara di Stasiun Dingling")
 
+scatterplot(df_qualityair)
+
 st.write(
     """
-         Berdasarkan visualisasi scatter matriks di bawah, diketahui bahwa:
+         Berdasarkan visualisasi scatter matriks di atas, diketahui bahwa:
 
         - `PM2.5` dan `PM10` terlihat dengan jelas membentuk sebuah garis mengarah ke kanan atas, menunjukkan korelasi positif yang kuat.
         - `CO` dan `NO2` membentuk persebaran mengarah ke kanan atas, persebarannya agak tersebar, menunjukkan korelasi positif yang sedang.
@@ -58,37 +115,13 @@ st.write(
          """
 )
 
-# Membuat scatter matrix
-scatter_data = df_a[["PM2.5", "PM10", "SO2", "NO2", "CO", "O3"]]
-scatter_matrix = pd.plotting.scatter_matrix(
-    scatter_data, figsize=(12, 12), diagonal="kde"
-)
-
-# Memodifikasi plot
-for i in range(len(scatter_data.columns)):
-    for j in range(len(scatter_data.columns)):
-        ax = scatter_matrix[i, j]
-        ax.xaxis.label.set_rotation(45)
-        ax.yaxis.label.set_rotation(0)
-        ax.yaxis.label.set_ha("right")
-        ax.xaxis.label.set_size(10)
-        ax.yaxis.label.set_size(10)
-
-# Menambah judul
-scatter_matrix[0, 0].get_figure().suptitle(
-    "Scatter Plot Matrix Parameter Kualitas Udara di Stasiun Pemantauan Dingling"
-)
-
-# Menampilkan plot di Streamlit
-st.pyplot(scatter_matrix[0, 0].get_figure())
-
 st.write(
     "##### Grafik Rata-Rata Nilai Tiap Kolom per Hari di Stasiun Pemantauan Dingling"
 )
 
 st.write(
     """
-        Berdasarkan visualisasi di bawah, diketahui bahwa:
+        Berdasarkan visualisasi di atas, diketahui bahwa:
 
         - `TEMP`, `PRES`, dan `DEWP` memiliki pola musiman, di mana `TEMP` dan `DEWP` memiliki pola garis yang cenderung mirip.
         - `PRES` terhadap `TEMP` dan `DEWP` memiliki pola garis yang berlawanan.
@@ -96,18 +129,7 @@ st.write(
         """
 )
 
-daily_mean = df_b.groupby(df_b["date"].dt.date).mean()
-fig, ax = plt.subplots(figsize=(18, 6))
-for column in daily_mean.columns:
-    if column != "date":
-        ax.plot(daily_mean.index, daily_mean[column], label=column)
-
-ax.set_title(
-    "Grafik Rata-Rata Nilai Tiap Kolom per Hari di Stasiun Pemantauan Dingling"
-)
-ax.set_xlabel("Tanggal")
-ax.set_ylabel("Nilai Rata-Rata")
-ax.legend()
-st.pyplot(fig)
+daily_mean = groupbydate(df_variable)
+linechart(daily_mean)
 
 st.caption("Oleh Ahmad Fadli Ramadhan")
